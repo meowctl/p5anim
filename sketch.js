d833ead5d0f79c5d1af80a220fdb2c9a6da3b666
lulu <meowctl@proton.me>
2026-06-03 08:15:47 -0300

    change asset frame order and add speed variable

--- sketch.js
+++ sketch.js
@@ -2,11 +2,11 @@ var img = [];
 
 function preload() {
     var i = 0;
-    for (var j = 1; j <= 8; i++, j++) {
-        img[i] = loadImage("assets/Dead (" + j + ").png");
-    }
     for (var j = 1; j <= 10; i++, j++) {
         img[i] = loadImage("assets/Idle (" + j + ").png");
+    }
+    for (var j = 1; j <= 10; i++, j++) {
+        img[i] = loadImage("assets/Walk (" + j + ").png");
     }
     for (var j = 1; j <= 12; i++, j++) {
         img[i] = loadImage("assets/Jump (" + j + ").png");
@@ -14,8 +14,8 @@ function preload() {
     for (var j = 1; j <= 8; i++, j++) {
         img[i] = loadImage("assets/Run (" + j + ").png");
     }
-    for (var j = 1; j <= 10; i++, j++) {
-        img[i] = loadImage("assets/Walk (" + j + ").png");
+    for (var j = 1; j <= 8; i++, j++) {
+        img[i] = loadImage("assets/Dead (" + j + ").png");
     }
 }
 
@@ -23,8 +23,13 @@ function setup() {
     createCanvas(400, 400);
 }
 
+// fps alvo
+var speed = 7;
+
 function draw() {
     background(220);
-    frameRate(30);
-    image(img[frameCount % img.length], 140, 50, 200, 200);
+
+    var mult = speed / getTargetFrameRate();
+    var frame = Math.floor(((frameCount-1) * mult) % img.length);
+    image(img[frame], 140, 50, 200, 200);
 }
